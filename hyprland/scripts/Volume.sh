3f334ee2a0b1ed3186e6f55abde6940b0be9228f
carbon <3035500479@qq.com>
2026-01-12 22:48:49 +0800

    update hypr

--- hyprland/scripts/Volume.sh
+++ hyprland/scripts/Volume.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Scripts for volume controls for audio and mic 
 
 iDIR="$HOME/.config/swaync/icons"
 sDIR="$HOME/.config/hypr/scripts"
--- hyprland/scripts/WaybarScripts.sh
+++ hyprland/scripts/WaybarScripts.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  #
+# This file used on waybar modules sourcing defaults set in $HOME/.config/hypr/UserConfigs/01-UserDefaults.conf
+
+# Define the path to the config file
 config_file=$HOME/.config/hypr/UserConfigs/01-UserDefaults.conf
 
 # Check if the config file exists
